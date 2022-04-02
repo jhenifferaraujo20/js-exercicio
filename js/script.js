668ad96801a1bbcbf5cc9a39ffb6fa7d67a1d8a2
Jheniffer Araujo <jhenifferbaldi@gmail.com>
2022-04-02 11:59:00 -0300

    Exercício

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let nome = prompt("Qual é o seu nome?");
+let idade = prompt("Qual é a sua idade?");
+let email = prompt("Qual é o seu email?");
+
+alert(`Nome: ${nome}, Idade: ${idade}, Email: ${email}`);
